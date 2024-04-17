ac817cfa3b79f55543b60e599a9efa8284159d3c
Krzysztof Krystian Jankowski <kj@p1x.in>
2024-04-17 03:33:59 +0200

    gravity + automatic running

--- bootsector/game.asm
+++ bootsector/game.asm
@@ -38,11 +38,54 @@ demoloop:
     rep stosb
 
     call draw_player
-    call draw_all_coins
     call handle_keyboard
+
+    cmp word [player_y], 172
+    je run
+    add word [player_y], 5
+    jmp no_run
+
+    run:
+    cmp byte [mirror_direction], 0
+    je run_right
+
+    run_left:
+        cmp word [player_x], 0
+        je swap_direction
+        dec word [player_x]
+        jmp no_run
+    
+    run_right:
+        cmp word [player_x], 316
+        je swap_direction
+        inc word [player_x]
+        jmp no_run
+
+        swap_direction:
+        xor byte [mirror_direction], 1  ; swap direction
+    no_run:
+        call simple_delay
+    
 
 jmp demoloop
 
+simple_delay:
+    push ax   ; Save AX register
+    push cx   ; Save CX register
+
+outer_loop:
+    mov cx, 0x00AF  ; Set the inner loop counter to its maximum value for maximum delay
+
+inner_loop:
+    dec cx          ; Decrement CX
+    jnz inner_loop  ; Continue inner loop until CX is zero
+
+    dec dx          ; Decrement DX
+    jnz outer_loop  ; Continue outer loop until DX is zero
+
+    pop cx          ; Restore CX register
+    pop ax          ; Restore AX register
+    ret             ; Return from the function
 
 draw_player:
 
@@ -101,105 +144,23 @@ draw_player:
     skip_reset:
     ret
 
-draw_coin:
-    mov ax, 32*3             ; Calculate frame offset (data is after 4th frame)
-    mov si, sprite_data
-    add si, ax             ; SI points to the start of the current frame data
-
-    mov ax, 320            ; Screen width
-    mul word [coin_y]      ; y-coordinate
-    add ax, [coin_x]       ; Add x-coordinate
-    mov di, ax             ; Store in DI for ES:DI addressing
-    mov cx, 4              ; 3 rows
-    draw_coin_row:
-        push cx
-        mov cx, 3           ; 4 pixels per row
-        rep movsb           ; Move sprite row to video memory
-        pop cx
-        add di, 317         ; Move DI to the start of the next line (320 - 4)
-        loop draw_coin_row
-    ret
-
-
-draw_all_coins:
-    mov bx, 0                ; Index for coin_positions
-    mov cx, [num_coins]      ; Load the number of coins
-
-draw_next_coin:
-
-    ; Skip this coin if it has been collected
-    cmp byte [coin_collected + bx], 1
-    je skip_coin
-
-    ; Load position from coin_positions array
-    mov ax, [coin_positions + bx]      ; Load x
-    mov [coin_x], ax
-    mov ax, 174  ; Load y
-    mov [coin_y], ax
-
-    ; check collision
-    mov ax, [player_x]
-    add ax, 4                           ; ax = player_x + player_width
-    cmp ax, [coin_x]
-    jle no_overlap                      ; No overlap if player_x + width <= coin_x
-
-    mov ax, [coin_x]
-    add ax, 3                           ; ax = coin_x + coin_width
-    cmp ax, [player_x]
-    jle no_overlap                      ; No overlap if coin_x + width <= player_x
-
-    ; flag coin as collected
-    mov byte [coin_collected + bx], 1
-    jmp skip_coin
-
-    no_overlap:
-        push cx
-        push bx
-        call draw_coin           ; Call the draw function
-        pop bx
-        pop cx
-
-    skip_coin:
-
-    add bx, 2                ; Move to the next coin position (each position is 2 bytes)
-    loop draw_next_coin      ; Decrement CX and loop if CX != 0
-    ret
-
-
 handle_keyboard:
-        ; Check if a key has been pressed
-        mov ah, 0x01
+        mov ah, 0x01            ; Check if a key has been pressed
         int 0x16
-        jz handle_keyboard  ; Jump if no key is in the keyboard buffer
+        jz no_kb                ; Jump if no key is in the keyboard buffer
 
-        mov ah, 0x00         ; Get the key press
+        mov ah, 0x00            ; Get the key press
         int 0x16
-        cmp ah, 0x4B       ; Compare with scan code for left arrow
-        je move_left
-        cmp ah, 0x4D       ; Compare with scan code for right arrow
-        je move_right
-        jmp handle_keyboard ; Unknown button, loop back
-
-    move_left:
-        dec word [player_x]
-        mov word [mirror_direction], 1
-        ret
-
-    move_right:
-        inc word [player_x]
-        mov word [mirror_direction], 0
+        xor byte [mirror_direction], 1  ; swap direction
+        
+    no_kb:
         ret
 
 .data:
-player_x dw 50
-player_y dw 172
+player_x dw 160
+player_y dw 12
 current_frame dw 0
 mirror_direction dw 0
-coin_x dw 0
-coin_y dw 0
-coin_positions dw 10, 30, 70, 90, 120, 140, 200, 210, 240
-coin_collected dw 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-num_coins dw 7
 
 sprite_data:
     ; Dude - Frame 1
@@ -208,32 +169,28 @@ sprite_data:
     db 0x2A, 0x5A, 0x0F, 0x66
     db 0x66, 0x42, 0x66, 0x66
     db 0x66, 0x34, 0x35, 0x66
-    db 0x66, 0x34, 0x35, 0x66
+    db 0x66, 0x35, 0x34, 0x66
     db 0x66, 0x36, 0x36, 0x66
     db 0x36, 0x66, 0x37, 0x66
     ; Dude - Frame 2
+    db 0x66, 0x66, 0x66, 0x66
     db 0x66, 0x2A, 0x2B, 0x66
     db 0x2A, 0x5A, 0x0F, 0x66
     db 0x66, 0x42, 0x66, 0x66
     db 0x66, 0x34, 0x35, 0x66
     db 0x66, 0x34, 0x35, 0x66
     db 0x66, 0x36, 0x36, 0x66
-    db 0x66, 0x36, 0x37, 0x37
-    db 0x66, 0x36, 0x66, 0x66
+    db 0x66, 0x36, 0x37, 0x66
     ; Dude - Frame 4
-    db 0x66, 0x66, 0x66, 0x66
-    db 0x2A, 0x2A, 0x2B, 0x66
-    db 0x66, 0x5A, 0x0F, 0x66
+    db 0x66, 0x2A, 0x2B, 0x66
+    db 0x2A, 0x5A, 0x0F, 0x66
     db 0x66, 0x42, 0x66, 0x66
     db 0x66, 0x34, 0x35, 0x66
-    db 0x66, 0x34, 0x35, 0x66
+    db 0x34, 0x35, 0x35, 0x66
     db 0x66, 0x36, 0x36, 0x66
-    db 0x66, 0x36, 0x37, 0x66
-    ; Coin tile
-    db 0x66, 0x2c, 0x66
-    db 0x2c, 0x0f, 0x2b
-    db 0x2c, 0x0f, 0x2b
-    db 0x66, 0x2b, 0x66  
+    db 0x66, 0x36, 0x37, 0x37
+    db 0x66, 0x36, 0x66, 0x66
+
 
 ; make boodsector
 times 510 - ($ - $$) db 0  ; Pad remaining bytes to make 510 bytes
