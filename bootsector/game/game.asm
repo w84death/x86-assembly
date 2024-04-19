ac786fdd62ce30e92d62a5373ffe434e568cbcfb
Krzysztof Krystian Jankowski <kj@p1x.in>
2024-04-19 22:35:15 +0200

    4 levels + game restat

--- bootsector/game/game.asm
+++ bootsector/game/game.asm
@@ -14,11 +14,19 @@ start:
     mov es, ax
 
 restart_game:
-    mov word [player_pos], 320*6+160
+    mov word [player_pos], PLAYER_START
+    mov word [current_level], 0
+    mov word [mirror_direction], 0
+    jmp game_loop
 
-    ; mov ax, [CS:TIMER]
-    ; and ax, 100
-
+next_level:
+    mov word ax, [current_level]
+    cmp ax, 3
+    jl .inc_level
+        mov word [current_level], 0
+    .inc_level:
+        add word [current_level], 1
+    mov word [player_pos], PLAYER_START
 
 game_loop:
 
@@ -43,12 +51,18 @@ game_loop:
             loop .next_color
 
     draw_level:
-    lea si, [level_data] ; Point SI to the start of level data
+        mov bx, [current_level]
+        mov ax, 20
+        mul bx
+        mov si, level_data
+        add si, ax              ; Move to correct level
+    
+    mov byte [exit], 0
     .read_segment:
         lodsb               ; Load pos
         test ax, ax
         jz done             ; If 0, end of data
-        
+
         movzx ax, al          ; Zero-extend AL to AX for multiplication (x value)
         imul ax, 20           ; AX = x * 20
         mov bx, ax            ; Store x*20 result in BX temporarily
@@ -64,6 +78,13 @@ game_loop:
         imul ax, 20             ; width of platform
 
         mov bh, PLATFORM_COLOR
+        push ax
+            mov ax, [exit]
+            cmp ax, 0
+            jne  .normal_platform
+            mov bh, EXIT_COLOR
+            .normal_platform:
+        pop ax
         
         mov cx, 10           ; Platform height  
         .draw_platform:
@@ -83,14 +104,17 @@ game_loop:
             sub di, ax      ; Move line down
             pop cx          ; Restore loop counter
             loop .draw_platform
+
+        inc byte [exit]
+
         jmp .read_segment   ; Process next segment
     done:
     
-    mov cx, 2
+
     collision_check:
-        mov bx, SPRITE_HEIGHT
-        sub bx, 1
-        add ax, 320                   ; check 6px below sprite top pos
+        push cx
+        mov bx, SPRITE_SIZE
+        add ax, 320                   ; check below sprite top pos
         mul bx
         add word ax, [player_pos]      ; Add x-coordinate
 
@@ -99,31 +123,19 @@ game_loop:
 
         cmp byte [mirror_direction], 0  ; Check direction
         jne .shift_col_check
-        add ax, 2                 
+        sub ax, SPRITE_SIZE/2                 
         jmp .after_shift                  
         .shift_col_check:
-        add ax, 2
+        add ax, SPRITE_SIZE/2
         .after_shift:
         mov di, ax              ; Store in DI for ES:DI addressing
         mov ah, es:[di]         ; Check if platform
         cmp ah, PLATFORM_COLOR
-        je run
+        jna run
+        cmp ah, EXIT_COLOR
+        ja next_level
         add word [player_pos], 320  ; Fall 1px down
-        ; loop collision_check
-
-        jmp run.continue_run
-
-    run:       
-        mov ax, [player_pos]    ; Load the player's buffer position into AX
-        mov cx, 320             ; Screen width into CX
-        xor dx, dx              ; Clear DX to prevent errors in division
-        div cx                  ; AX now contains quotient, DX contains remainder (player's x-coordinate)
-
-        cmp dx, 2               ; Check if at the left boundary
-        je  .swap_direction      ; Jump if exactly at the left edge
-        cmp dx, 319 - SPRITE_WIDTH            ; Check if at or beyond the right boundary (319 for a 320px wide screen)
-        je  .swap_direction     ; Jump if at or beyond the right edge
-       
+    run:
         .check_run_dir:
             cmp byte [mirror_direction], 0
             je .run_right
@@ -132,10 +144,6 @@ game_loop:
             jmp .continue_run
         .run_right:
             inc word [player_pos]
-            jmp .continue_run
-        .swap_direction:
-            xor byte [mirror_direction], 1  ; swap direction
-            jmp .check_run_dir
         .continue_run:
         
     .handle_keyboard:
@@ -146,78 +154,52 @@ game_loop:
         int 0x16
         cmp ah, 0x01            ; Check if the scan code is for the [Esc] key
         je  restart_game
+        cmp ah, 0x1C            ; Check if the scan code is for the [Esc] key
+        je  next_level
 
         xor byte [mirror_direction], 1  ; swap direction
     .no_kb:
 
     draw_player:
-        mov bx, [current_frame]  ; Load current frame number
-
-        mov ax, 28             ; Frame offset
-        mul bx                 ; AX = 28 * current frame number
-        mov si, sprite_data
-        add si, ax             ; SI points to the start of the current frame data
-
-        ; Calculate the starting address in video memory
         mov word ax, [player_pos]     ; Add x-coordinate
         mov di, ax             ; Store in DI for ES:DI addressing
-    
-    ; Check mirror direction
-        cmp byte [mirror_direction], 0
-        je draw_normal
-        add di, 4
-        mov cx, SPRITE_HEIGHT
-        draw_mirrored:
-            push cx
-            ; lea bx, [si+SPRITE_WIDTH-1]
-            mov cx, SPRITE_WIDTH
-            push di             ; Save DI before drawing each row
-            .draw_pixel:
-                push cx
-                lodsb
-                test al, al
-                jz .skip_pixel
-                stosb
-                jmp .continue_pixel
-                .skip_pixel:
-                    inc di 
-                .continue_pixel:
-                    add di, -2
-                pop cx
-            loop .draw_pixel
-            pop di              ; Restore DI from the saved value before drawing each row
-            add di, 320        ; Move DI to the start of the next line
-            pop cx
-            loop draw_mirrored
-        jmp finish_draw
 
-        draw_normal:
-            mov cx, SPRITE_HEIGHT
-            .draw_row:
-                push cx
-                mov cx, SPRITE_WIDTH
-                .draw_pixel:
-                    push cx
-                    lodsb
-                    test al, al
-                    jz .skip_pixel
-                    stosb
-                    jmp .continue_pixel
-                    .skip_pixel:
-                        inc di
-                    .continue_pixel:
-                    pop cx
-                    loop .draw_pixel
-                pop cx
-                add di, 320 - SPRITE_WIDTH         ; Move DI to the start of the next line
-                loop .draw_row
-
-    finish_draw:
-        inc byte [current_frame]
-        cmp byte [current_frame], 3
-        jl .skip_reset
-        mov byte [current_frame], 0
-        .skip_reset:
+        mov bh, PLAYER_COLOR
+        mov ax, 1
+        mov cx, SPRITE_SIZE
+        .draw_row:
+            push cx         ; Save loop counter
+            push ax         ; Save length
+            
+            mov cx, ax     
+            mov al, bh
+            add bh, 1
+            rep stosb
+            pop ax
+            cmp ax, SPRITE_SIZE/2
+            jge .skip_enlarge
+            add di, 321       
+            add ax, 2
+            
+            jmp .finish_row
+            .skip_enlarge:
+            add di, 320
+            .finish_row:
+            sub di, ax      ; Move line down 
+            pop cx          ; Restore loop counter
+            loop .draw_row
+        .drawDir:
+            mov ax, 320*7
+            sub di, ax      ; Move up 4 lines
+            mov ax, -2
+            cmp byte [mirror_direction], 0  ; Check direction
+            jne .shifted
+            mov ax, 5
+            .shifted:
+            add di, ax  ; Move position left/right
+            mov cx, 4
+            mov al, 128
+            rep stosb 
 
     vga_blit:
         push es
@@ -245,53 +227,62 @@ jmp game_loop
 
 .data:
 player_pos dw 0
-current_frame dw 0
 mirror_direction db 0
+current_level dw 0
+exit dw 0
+PLAYER_COLOR equ 53
 SKY_COLOR equ 82
 PLATFORM_COLOR equ 70
-SPRITE_WIDTH equ 4
-SPRITE_HEIGHT equ 7
+EXIT_COLOR equ 90
+SPRITE_SIZE equ 12
 DEATH_ROW equ 194
 TIMER equ 046Ch
+PLAYER_START equ 320*6+160
 
-sprite_data: ;84b
-    ; Girl - Frame 1 / 28b
-    db 0, 0, 0, 0, 0, 
-    db 42, 42, 0
-    db 42, 90, 15, 0
-    db 0, 88, 0, 0
-    db 0, 59, 83, 0
-    db 0, 59, 34, 0
-    db 33,0,32,0
-    ; Girl - Frame 2 / 28b
-    db 0, 42, 42, 0
-    db 42, 90, 15, 0
-    db 0, 88, 0, 0
-    db 0, 59, 83, 0
-    db 0, 59, 34, 0
-    db 0,55,55,33
-    db 0,32,0,0
-    ; Girl - Frame 3 / 28b
-    db 42, 42, 42, 0
-    db 0, 90, 15, 0
-    db 0, 88, 0, 0
-    db 0, 59, 83, 0
-    db 0, 59, 34, 0
-    db 0,55,55,0
-    db 0,33,32,0
-
-level_data: ; 14b
+level_data: ; 20b
+    db 184, 5
+    db 23, 3
+    db 83, 11
+    db 160, 7
+    db 137, 7
+    db 0, 0 ; End marker
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0, 0
+level_2: ; 20b
+    db 203, 3
     db 23, 2
     db 54, 4
     db 82, 4
     db 88, 4
     db 123, 3
-    db 132, 2
     db 135,3
-    db 177,0xb
-    db 199,9
+    db 162,4
+    db 169,3
     db 0, 0 ; End marker
-
+level_3:
+    db 19, 9
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0,0
+    db 0,0
+    db 0,0
+    db 0, 0 ; End marker
+level_4:
+    db 22, 4
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0, 0
+    db 0,0
+    db 0,0
+    db 0,0
+    db 0, 0 ; End marker
 ; make boodsector
 times 510 - ($ - $$) db 0  ; Pad remaining bytes to make 510 bytes
 dw 0xAA55                  ; Boot signature at the end of 512 bytes
