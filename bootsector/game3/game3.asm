b4d7c084b0e750bd922a968ee0f1af0d3d6da478
Kzysztof Krystian Jankowski <kj@p1x.in>
2024-05-16 21:04:03 +0200

    game 3 - RC1

--- bootsector/game3/game3.asm
+++ bootsector/game3/game3.asm
@@ -7,12 +7,14 @@
 
 cpu pentium
 
-; ======== MEMORY MAP ========
+
+; =========================================== MEMORY ===========================
 
 VGA equ 0xA000
 TIMER equ 0x046C                            ; BIOS timer
+VGA_BUFFER equ 0xFA00                       ; Double buffer
 
-; ======== SETTINGS ========
+; =========================================== MAGIC NUMBERS ====================
 
 SCREEN_WIDTH equ 320                        ; 320x200 pixels
 SCREEN_HEIGHT equ 200
@@ -20,22 +22,23 @@ SCREEN_CENTER equ SCREEN_WIDTH*SCREEN_HEIGHT/2+SCREEN_WIDTH/2
 
 SPRITE_SIZE equ 8                           ; 8 pixels per sprite line
 SPRITE_LINES equ 7                          ; 7 lines per sprite  
-MAX_ENEMIES equ 64
-MAX_FLOWERS equ 8
-ENEMIES_PER_LEVEL equ 4
+MAX_ENEMIES equ 64                          ; Maximum number of enemies
+MAX_FLOWERS equ 8                           ; Maximum number of flowers            
+ENEMIES_PER_LEVEL equ 4                     ; Number of enemies per level
 
 COLOR_BG equ 20                                 
 COLOR_SPIDER equ 0
-COLOR_FLOWER equ 13
+COLOR_FLOWER equ 10
 COLOR_FLY equ 77
 
-SPRITE_FLY equ 0
-SPRITE_SPIDER equ 14
-SPRITE_FLOWER equ 28
+SPRITE_FLY equ 0                            ; Fly sprite ID (position in memory)
+SPRITE_SPIDER equ 14                        ; Spider sprite ID
+SPRITE_FLOWER equ 28                        ; Flower sprite ID
 
+; =========================================== RESERVE MEMORY ===================
 
 section .bss
-    BUFFER resb 64000
+    BUFFER resb VGA_BUFFER
     LIFE resb 1
     LEVEL resw 1
     SPRITE resw 1
@@ -43,10 +46,12 @@ section .bss
     PLAYER resb 5
     ENTITIES resb MAX_ENEMIES*5+MAX_FLOWERS*5
 
+; =========================================== IMPLEMENTATION ===================
+
 section .text
     global _start
 
-; ======== GRAPHICS INITIALIZATION ========
+; =========================================== BOOTSTRAP ========================
 
 _start:
     xor ax,ax                               ; Init segments (0)
@@ -59,7 +64,7 @@ _start:
     mov ax, BUFFER                          ; Set double buffer
     mov es, ax                              ; as target
 
-; ======== GAME RESET/INIT ========
+; =========================================== GAME INITIALIZATION / RESET ======
 
 restart_game:
     mov byte [LIFE], 3                      ; Starting lifes
@@ -72,6 +77,8 @@ restart_game:
         mov byte [si], 0                    ; Clear sprite ID
         add si, 5                           ; Move to next memory position
     loop .clear_entites
+
+; =========================================== LEVEL INITIALIZATION / NEXT LEVEL
 
 next_level:
     mov word [PLAYER+3], SCREEN_CENTER      ; Position
@@ -104,11 +111,12 @@ next_level:
     add si, 5                               ; Move to next memory position
     loop .spawn_flowers
 
-; ======== GAME LOOP  ========
+
+; =========================================== MAIN GAME LOOP ===================
 
 game_loop:
 
-; ======== CLEAR / DRAW BACKGROUND ========
+; =========================================== DRAW BACKGROUND ==================
 
 draw_bg:
     xor di,di
@@ -124,7 +132,8 @@ draw_bg:
         dec dx                              ; Decrement bar counter
         jnz .draw_bars                      ; Repeat for all bars
 
-; ======== DRAW ENTITES ========
+
+; =========================================== DRAW ENTITIES ====================
 
 draw_entities:
     mov word cx, MAX_ENEMIES
@@ -152,6 +161,9 @@ draw_entities:
                 movzx si, [si+2]            ; Direction            
                 shl si, 1
                 add di, [MLT + si]          ; Movement Lookup Table
+                cmp di, VGA_BUFFER               ; Clip screen size (VGA_BUFFER is the hexadecimal representation of 64000)
+                jb .draw_entitie
+                and di, VGA_BUFFER               ; Clip screen size 
         .draw_entitie:
             push di
             mov byte BL, [COLOR]
@@ -160,7 +172,7 @@ draw_entities:
             call draw_sprite
             pop di
         pop si
-        mov word [si+3], DI                 ; Save new position
+        mov word [si+3], di                 ; Save new position
 
         .random_rotate:
             rdtsc                           ; Randomize rotation
@@ -174,6 +186,9 @@ draw_entities:
         pop cx
         loop .next
         .done:
+
+
+; =========================================== COLLISION CHECKING ===============
 
 check_collisions:
     mov di, [PLAYER+3]                      ; Player position
@@ -207,7 +222,9 @@ check_collisions:
         jmp next_level
 
     .collision_done:
-    
+
+; =========================================== PLAYER MOVEMENT ==================
+
 handle_player:
     mov di, [PLAYER+3]                      ; Position
     mov byte BL, [PLAYER+1]
@@ -226,135 +243,90 @@ handle_player:
     call draw_sprite                        ; Draw player sprite
 
 
-; ======== CHECKING KEYBOARD ========
+; =========================================== KEYBOARD INPUT ===================
+
 
 handle_keyboard:
-    mov ah, 0x01            ; Check if a key has been pressed
-    int 0x16
-    jz .no_move             ; No press
-    xor ax,ax               ; Get the key press
-    int 0x16
+    mov ah, 0x01                            ; Check if a key has been pressed
+    int 0x16                                ; Get the key press
+    jz .no_move                             ; No press
+    xor ax,ax                               ; Clear AX
+    int 0x16                                ; Get the key press code
     .rotate_player:
-        mov byte bl, [PLAYER+2]   ; Get current rotation 0-7
-        inc bl                      ; Move rotation clockvise
-        and bl, 7                   ; Limit 0..7
-        mov byte [PLAYER+2], bl   ; Save back
+        mov byte bl, [PLAYER+2]             ; Get current rotation 0-7
+        inc bl                              ; Move rotation clockvise
+        and bl, 7                           ; Limit 0..7
+        mov byte [PLAYER+2], bl             ; Save back
     .no_move:
 
+; =========================================== VGA SPILT ========================
 
 vga_blit:
     push es
     push ds
  
-    mov ax, 0xA000  ; VGA memory
-    mov bx, BUFFER  ; Buffer memory
-    mov es, ax
-    mov ds, bx
-    mov cx, 16000   ; Quater of buffer
-    xor si, si
-    xor di, di
-    rep movsd       ; Push double words (4x pixels)
+    mov ax, VGA                             ; VGA memory
+    mov bx, BUFFER                          ; Buffer memory
+    mov es, ax                              ; Set VGA memory as target
+    mov ds, bx                              ; Set buffer memory as source
+    mov cx, 0x3E80                          ; Quater of buffer (hex value)
+    xor si, si                              ; Clear SI
+    xor di, di                              ; Clear DI
+    rep movsd                               ; Push double words (4x pixels)
 
     pop ds
     pop es
 
-; ======== DELAY CYCLE ========
+; =========================================== DELAY CYCLE ======================
 
 delay_timer:
-    mov ax, [TIMER]
-    inc ax              ; 1 cycle
+    mov ax, [TIMER]                         ; Get current timer value
+    inc ax                                  ; Increment it by 1 cycle (42ms)
     .wait:
-        cmp [TIMER], ax
-        jl .wait
+        cmp [TIMER], ax                     ; Compare with the current timer value
+        jl .wait                            ; Loop until equal
 
 jmp game_loop
 
-; ======== DRAWING SPRITE PROCEDURE ========
+; =========================================== DRAWING SPRITE PROCEDURE =========
+
 
 draw_sprite:
-    MOV DX, SPRITE_LINES    ; Number of lines in the sprite
+    MOV DX, SPRITE_LINES                    ; Number of lines in the sprite
     .draw_row:
-        PUSH DX             ; Save DX
-        MOV AL, [SI]        ; Get sprite row data
-        MOV AH, 0           ; Clear AH
-        MOV CX, 8           ; 8 bits per row
-
-    .draw_pixel:
-        SHL AL, 1           ; Shift left to get the next bit into carry flag
-        JNC .skip_pixel     ; If carry flag is 0, skip setting the pixel
-        MOV [ES:DI], BL     ; Set the pixel
-
-    .skip_pixel:
-        INC DI              ; Move to the next pixel position horizontally
-        LOOP .draw_pixel    ; Repeat for all 8 pixels in the row
-
-        POP DX              ; Restore DX
+        PUSH DX                             ; Save DX
+        MOV AL, [SI]                        ; Get sprite row data
+        MOV AH, 0                           ; Clear AH
+        MOV CX, 8                           ; 8 bits per row
+        .draw_pixel:
+            SHL AL, 1                       ; Shift left to get the next bit into carry flag
+            JNC .skip_pixel                 ; If carry flag is 0, skip setting the pixel
+            MOV [ES:DI], BL                 ; Set the pixel
+        .skip_pixel:
+            INC DI                          ; Move to the next pixel position horizontally
+            LOOP .draw_pixel                ; Repeat for all 8 pixels in the row
+        POP DX                              ; Restore DX
         INC SI
-        ADD DI, 320         ; Move to the next line in the video buffer
-        SUB DI, 8           ; Adjust DI back to the start of the line
-        DEC DX              ; Decrement row count
-        JNZ .draw_row       ; If there are more rows, draw the next one
+    ADD DI, 312                             ; Move to the next line in the video buffer
+    DEC DX                                  ; Decrement row count
+    JNZ .draw_row                           ; If there are more rows, draw the next one
     ret
 
 
-; ======== DATA ========
+; =========================================== DATA =============================
 
-MLT dw -320,-319,1,321,320,319,-1,-321  ; Movement Lookup Table
-
+MLT dw -320,-319,1,321,320,319,-1,-321      ; Movement Lookup Table
 sprites:
-; fly
-db 01100000b
-db 10010110b
-db 01001001b
-db 00110010b
-db 01011100b
-db 01111101b
-db 00011110b
-
-db 00000000b
-db 00000000b
-db 00011110b
-db 01110010b
-db 01011100b
-db 01111101b
-db 00011110b
-
-; spider
-db 00000110b
-db 01110111b
-db 10101111b
-db 11111110b
-db 00101010b
-db 01001001b
-db 01001001b
-
-db 00000110b
-db 01110111b
-db 10101111b
-db 11111110b
-db 00101011b
-db 11010100b
-db 00010100b
-
-; flower
-db 00011100b
-db 00110110b
-db 00011100b
-db 01101000b
-db 00111100b
-db 00001000b
-db 00001000b
-
-db 00111000b
-db 01101100b
-db 00111000b
-db 01101000b
-db 00111100b
-db 00001000b
-db 00001000b
+db 0x60, 0x96, 0x49, 0x32, 0x5C, 0x7D, 0x1E ; Fly sprite frame 0
+db 0x00, 0x00, 0x1E, 0x72, 0x5C, 0x7D, 0x1E ; Frame 1
+db 0x06, 0x77, 0xAF, 0xFE, 0x2A, 0x49, 0x49 ; Spider sprite frame 0
+db 0x06, 0x77, 0xAF, 0xFE, 0x2B, 0xD4, 0x14 ; Frame 1
+db 0x1C, 0x36, 0x1C, 0x48, 0x3F, 0x08, 0x08 ; Flower sprite frame 0
+db 0x38, 0x6C, 0x38, 0x09, 0x7E, 0x08, 0x08 ; Frame 1
 
 
-; ======== BOOTSECTOR  ========
-times 507 - ($ - $$) db 0  ; Pad remaining bytes
-p1x db 'P1X'            ; P1X signature 4b
-dw 0xAA55
+; =========================================== BOOTSECTOR =======================
+
+times 507 - ($ - $$) db 0                   ; Pad remaining bytes
+db 'P1X'                                    ; P1X signature 3b
+dw 0xAA55                                   ; Boot signature    
