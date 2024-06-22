f442dae43a1c461da166ebd63fa7df24410b69d2
Krzysztof Krystian Jankowski <kj@p1x.in>
2024-06-22 19:16:43 +0200

    game7 update (DOS)

--- bootsector/game7/game7.asm
+++ bootsector/game7/game7.asm
@@ -3,19 +3,18 @@ use16
 
 VGA_MEMORY_ADR equ 0xA000                   ; VGA memory address
 DBUFFER_MEMORY_ADR equ 0x8000               ; Doublebuffer memory address
-SCREEN_BUFFER_SIZE equ 0xFa00               ; Size of the VGA buffer size
-TIMER equ 0x046C                            ; BIOS timer
+
 SPRITE_WIDTH equ 0x08
 SPRITE_LINES equ 0x08
+
 STATE_ADR equ 0xfe00
 STATE_ON equ 0x01
 STATE_OVER equ 0x02
 STATE_NEWALIEN equ 0x03
 ; game variables 01-0f
 HEALTH_ADR equ 0x7e01                       ; 1 byte
-WEAPON_SHOOT_ADR equ 0x7e02                 ; 1 byte
-HITS_ADR equ 0x7e03
-WEAPON_AIM_ADR equ 0x7e05
+HITS_ADR equ 0x7e02                         ; 2
+WEAPON_AIM_ADR equ 0x7e04                   ; 4
 ; aliens
 ALIENS_ADR equ 0x7e10
 
@@ -27,81 +26,77 @@ start:
     push DBUFFER_MEMORY_ADR                 ; Set doublebuffer memory
     pop es                                  ; as target
 
+
     mov ax, 7
-    mov cx,0 ; min pos
-    mov dx,320 ; max pos
+    mov cx, 1
+    mov dx,319
     int 33h
 
+restart_game:
     mov byte [STATE_ADR],STATE_ON
-    mov byte [HEALTH_ADR],0x04
+    mov byte [HEALTH_ADR],0x0a
     mov word [HITS_ADR],0x00
 
 
 create_aliens:
     mov si, ALIENS_ADR
-    mov word [si], 320*20+160
-    mov word [si+2],0x0000
+    mov word [si], 320*100+160
+    ;mov word [si+2], 0x0000
     mov word [si+4], 0xffff
 
 ; main loop
 game_loop:
-    mov ax,0x1112
-    mov cx,SCREEN_BUFFER_SIZE               ; Set buffer size to fullscreen
-    rep stosw                               ; Fill the buffer with color
+    xor di,di
+    mov cx, 200
+    .l:
+       push cx
+       xchg ax, cx
+       add ax, bp
+       shr ax, 2
+       and al,0x05
+       add al, 0x12
+       mov cx, 320
+       rep stosb
+       pop cx
+    loop .l
 
+    inc bp
 
 mouse_handler:
     mov ax, 0x0003
     int 0x33
-    cmp byte [WEAPON_SHOOT_ADR], 0x0
-    jnz .done
-        mov byte [WEAPON_SHOOT_ADR],al
-    .done:
     mov word [WEAPON_AIM_ADR], cx
     mov word [WEAPON_AIM_ADR+2], dx
-    mov bx, 320
-    mov ax, dx
-    mul bx
-    add ax, cx
-    mov di, ax
-    mov ax, 0x0f
+    ; draw crosshair dot
+    imul dx, 320
+    add dx, cx
+    mov di,dx
+    mov al,0x0f
     stosb
 
 
-; get aliens
+; draw aliens
 
-cmp byte [STATE_ADR], STATE_ON
-jne skip_aliens_loop
+;cmp byte [STATE_ADR], STATE_ON
+;jne skip_aliens_loop
 
-  mov si, ALIENS_ADR
-  ;xor bx,bx
+mov si, ALIENS_ADR
 aliens_loop:
 
   rdtsc
-  add ax, si
-  xor ax, 0x1337
-  and ax, 0x0f ; MLT size
+  and ax, 0x07 ; MLT size
   mov di, ax
   shl di, 1
   mov ax, [MLT+di]
   add ax, [MLT+di]
   mov di,[si]
   add di, ax
-  jnz .ok1
-  inc di
-  .ok1:
-  add ax, 320*40
-  cmp di, 320*160
-  jb .ok2
-  and di, 320*160
-  .ok2:
   mov [si],di
 
   inc si
   inc si
 
   ; check collisions
-
   mov ax,di    ; linear pos
   mov bx,320
   xor dx,dx
@@ -121,6 +116,10 @@ aliens_loop:
   jg .no_hit
 
   inc word [HITS_ADR]
+  mov word ax, [HITS_ADR]
+  ; check for win
+  and al, 0x05
+  jnz .hit
   mov byte [STATE_ADR],STATE_NEWALIEN
   jmp .hit
 
@@ -133,18 +132,21 @@ aliens_loop:
       dec byte [HEALTH_ADR]
       cmp byte [HEALTH_ADR], 0x0
       ja .game_on
-      mov byte [STATE_ADR], STATE_OVER
+        .wait_for_esc:
+        in ax, 0x60
+        dec ax
+        jnz .wait_for_esc
+        jmp restart_game
       .game_on:
       .hit:
       push si
-      mov bx, 0x04
+      mov bx, 0x28
       mov si, HitSpr
       call draw_sprite
       call vga_blit
       pop si
       rdtsc
-      and ax, 320*160
-      add ax, 320*20
+      and ax, 320*32
       mov [si-2], ax
       mov word [si], 0x0000
   .continue:
@@ -154,7 +156,13 @@ aliens_loop:
 
 ; draw sprite
   push si
-  mov si, UfoSpr
+  mov si, EnemySpr
+  mov ax, bx
+  shr ax,1
+  jnc .ok
+  add si, 8
+  .ok:
+
   call draw_sprite
   pop si
 
@@ -167,22 +175,16 @@ aliens_loop:
   cmp ax,0xffff
   jnz aliens_loop
 
-skip_aliens_loop:
-
-
 add_alien:
     mov byte al, [STATE_ADR]
     cmp al, STATE_NEWALIEN
     jne .skip
         mov byte [STATE_ADR], STATE_ON
-
         rdtsc
-        and ax, 320*160
-        add ax, 320*20
+        and ax, 320*32
         mov [si], ax
         mov word [si+2], 0x0000
         mov word [si+4], 0xffff
-
    .skip:
 
 draw_health_bar:
@@ -191,11 +193,11 @@ draw_health_bar:
     cmp cx,0x0
     jz .skip
     mov si, HealthSpr
-    mov di, 320*4+4
-    .l:
+    mov di, 320*4+80
     mov bx, 0x28
-    call draw_sprite
-    add di, 0x8
+    .l:
+        call draw_sprite
+        add di, 0x10
     loop .l
     .skip:
 
@@ -204,9 +206,9 @@ draw_hits_counter:
     cmp cx,0x0
     jz .skip
     mov si, OneMoreSpr
-    mov di, 320*4+4+56
+    mov di, 320*190+4
     .l:
-    mov bx, 0x0f
+    mov bx, 0x1e
     call draw_sprite
     add di, 0x3
     loop .l
@@ -216,15 +218,20 @@ draw_hits_counter:
 
 ; =========================================== DELAY CYCLE ======================
 
-delay_timer:
-    mov ax,[TIMER]                          ; Get current timer value
-    inc ax                                  ; Increment it by 1 cycle (42ms)
-    .wait:
-        cmp [TIMER],ax                      ; Compare with the current timer
-        jl .wait                            ; Loop until equal
+delay:
+    push es
+    push 0x0040
+    pop es
+    mov bx, [es:0x006C]  ; Load the current tick count into BX
+wait_for_tick:
+    mov ax, [es:0x006C]  ; Load the current tick count
+    sub ax, bx           ; Calculate elapsed ticks
+    jz wait_for_tick     ; If not enough time has passed, keep waiting
+    pop es
 
 
-    jmp game_loop
+jmp game_loop
+
 
 ; =========================================== VGA BLIT PROCEDURE ===============
 
@@ -245,6 +252,7 @@ vga_blit:
     pop es
     ret
 
+
 ; =========================================== DRAWING SPRITE PROCEDURE =========
 
 draw_sprite:
@@ -261,7 +269,7 @@ draw_sprite:
             inc di                          ; Move to the next pixel position
             loop .draw_pixel                ; Repeat for all 8 pixels in the row
         inc si
-    add di,320-SPRITE_WIDTH                 ; Move to the next line
+    add di,320-SPRITE_WIDTH                              ; Move to the next line
     dec dx                                  ; Decrement row count
     jnz .draw_row                           ; Draw the next row
     popa
@@ -270,18 +278,31 @@ draw_sprite:
 
 ; Data segment
 
-MLT dw -322,-318,318,322,-1,1,-1,1,-1,-1,1,1,-1,-1,1,1      
-                                            ; Movement Lookup Table
+MLT dw 1,-1,318,322,-1,1,-2,2      ; Movement Lookup Table
+                                            ; 0 - up/left
+                                            ; 1 - up/right
+                                            ; 2 - down/left
+                                            ; 3 - down/right
 
-UfoSpr:
-db 10111101b
+EnemySpr:
+; Frame 1
+db 11000011b
 db 01011010b
-db 11100111b
-db 10011001b
-db 11111111b
 db 00111100b
 db 01011010b
-db 11100111b
+db 11111111b
+db 10100101b
+db 10011001b
+db 01000010b
+; Frame 2
+db 00000000b
+db 01011010b
+db 11111111b
+db 01011010b
+db 11111111b
+db 10100101b
+db 10011001b
+db 01000010b
 HitSpr:
 db 10010001b
 db 01000010b
@@ -292,14 +313,14 @@ db 00011000b
 db 01000010b
 db 10010001b
 HealthSpr:
-db 00111100b
-db 01100110b
-db 01000010b
-db 01100110b
 db 01111110b
-db 00111100b
-db 01000010b
-db 00111100b
+db 11000011b
+db 10011001b
+db 10111101b
+db 10111101b
+db 10011001b
+db 11000011b
+db 01111110b
 OneMoreSpr:
 db 00000100b
 db 00001000b
@@ -310,8 +331,8 @@ db 00100000b
 db 01000000b
 db 10000000b
 
-; =========================================== BOOT SECTOR ======================
 
-times 507 - ($ - $$) db 0                   ; Pad remaining bytes
-p1x db 'P1X'                                ; P1X signature 4b
-dw 0xAA55                                   ; Boot signature
+
+times 507-($-$$) db 0
+db "P1X"
+dw 0xAA55
