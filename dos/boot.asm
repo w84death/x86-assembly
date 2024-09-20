d544282ae28052a8045c70de9bda53e6d1f0847b
Krzysztof Krystian Jankowski <kj@p1x.in>
2024-09-21 00:57:29 +0200

    bootloader fix

--- dos/boot.asm
+++ dos/boot.asm
@@ -19,42 +19,37 @@ start:
     mov ds, ax
     mov es, ax
 
-    mov [boot_drive], dl   ; Save the boot drive number
-
 .clear_screen:
     mov ah, 0x06           ; BIOS scroll up function
     mov al, 0              ; Clear entire screen
-    mov bh, 0xf0           ; Text attribute
+    mov bh, 0x70           ; Text attribute
     mov cx, 0              ; Upper left corner
     mov dx, 0x184F         ; Lower right corner
     int 0x10               ; BIOS video interrupt
 
 .display_title:
-    mov dh, 5               ; Row
-    mov dl, 29              ; Column
+    mov dx, 0x051D          ; Row 5, Column 29
     call set_cursor
     mov si, welcome_msg
     call print_string
 
 .load_code:
-    mov dh, 10               ; Row
-    mov dl, 4              ; Column
+    mov dx, 0x0a04          ; Row 10, Column 4
     call set_cursor
     mov si, loading_msg
     call print_string
 
-    ; Set up ES:BX to point to 0x7E0:0x0100
     mov ax, 0x7E0          ; ES = 0x7E0
     mov es, ax
     mov bx, 0x0100         ; BX = 0x0100
 
-    ; Load next 4 sectors into ES:BX
+.load_sectors:
     mov ah, 0x02           ; BIOS read sectors function
     mov al, 4              ; Number of sectors to read
     mov ch, 0              ; Cylinder 0
     mov dh, 0              ; Head 0
     mov cl, 2              ; Sector 2 (sectors start at 1)
-    mov dl, [boot_drive]   ; Boot drive number
+    mov dl, 0               ; Boot drive number
     int 0x13               ; BIOS disk interrupt
     jc disk_error          ; Jump if carry flag set (error)
 
@@ -62,8 +57,8 @@ start:
     mov si, done_msg
     call print_string
 
-    mov dh, 12               ; Row
-    mov dl, 4              ; Column
+    mov dh, 12              ; Row
+    mov dl, 4               ; Column
     call set_cursor
     mov si, wait_msg
     call print_string
@@ -71,14 +66,12 @@ start:
     xor ax, ax
     int 0x16               ; BIOS keyboard interrupt (wait for keypress)
 
-    ; Set up segment registers for the loaded code
     mov ax, 0x7E0          ; Segment where code is loaded
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00         ; Initialize stack pointer  
 
-    ; Jump to the loaded code at 0x7E0:0x0100
     jmp 0x7E0:0x0100
 
 disk_error:
@@ -104,11 +97,10 @@ set_cursor:
     ret
 
 welcome_msg db '>>> P1X Bootloader <<<', 0
-loading_msg db 'Loading... ', 0
+loading_msg db 'Loading four sectors... ', 0
 done_msg db 'Done!', 0
 wait_msg db 'Press any key to start...', 0
 error_msg db 'Disk Read Error!', 0
-boot_drive db 0             ; Variable to store boot drive number
 
 times 507 - ($ - $$) db 0   ; Pad to 510 bytes
 db "P1X"                    ; Use HEX viewer to see P1X at the end of binary
