9f94cf39575965936e7b23e2baa96fd4305a2e9b
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-01-04 00:29:06 +0100

    upd

--- bios/game12/game12.asm
+++ bios/game12/game12.asm
@@ -18,8 +18,9 @@ _BASE_ equ 0x2000
 _GAME_TICK_ equ _BASE_ + 0x00       ; 2 bytes
 _GAME_STATE_ equ _BASE_ + 0x02      ; 1 byte
 _SCORE_ equ _BASE_ + 0x03           ; 1 byte
-_X_  equ _BASE_ + 0x04            ; 2 bytes
-_Y_  equ _BASE_ + 0x06            ; 2 bytes
+_CUR_X_  equ _BASE_ + 0x04          ; 2 bytes
+_CUR_Y_  equ _BASE_ + 0x06          ; 2 bytes
+_VECTOR_COLOR_ equ _BASE_ + 0x08    ; 1 byte
 
 _SPRITES_ equ 0x3000
 _LEVEL_ equ 0x4000
@@ -49,13 +50,12 @@ mov ax, 0xA000
 mov es, ax
 xor di, di          ; Set DI to 0
 
-; =========================================== GAME LOOP ========================
-
-mov word [_X_], 160
-mov word [_Y_], 100
-
+mov byte [_GAME_TICK_], 0
+mov byte [_SCORE_], 0
 mov byte [_GAME_STATE_], STATE_INTRO
-call draw_bg
+call prepare_intro
+
+; =========================================== GAME LOOP ========================
 
 game_loop:
 
@@ -95,68 +95,48 @@ check_keyboard:
       jmp .done
    .go_intro:
       mov byte [_GAME_STATE_], STATE_INTRO
-      call draw_bg
+      call prepare_intro
       jmp .done
    .go_game:
       mov byte [_GAME_STATE_], STATE_GAME
-      call draw_bg
+      call prepare_game
       jmp .done
    .pressed_up:
-      dec word [_Y_]
+      dec word [_CUR_Y_]
       jmp .done
    .pressed_down:
-      inc word [_Y_]
+      inc word [_CUR_Y_]
       jmp .done
    .pressed_left:
-      dec word [_X_]
+      dec word [_CUR_X_]
       jmp .done
    .pressed_right:
-      inc word [_X_]
+      inc word [_CUR_X_]
       jmp .done
 
    .done:
 
 ; =========================================== GAME STATES ======================
+
 cmp byte [_GAME_STATE_], STATE_QUIT
 je exit
 
 cmp byte [_GAME_STATE_], STATE_INTRO
-je the_intro
+je draw_intro
 
 cmp byte [_GAME_STATE_], STATE_GAME
-je the_game
+je draw_game
 
 jmp wait_for_tick
 
+draw_intro:
+   ; animate "PRESS ENTER TO START"
+   jmp wait_for_tick
 
-the_intro:
-mov ax, 20
-mov bx, 300
-mov dl, 80
-mov dh, 80
-mov cl, 0x14
-call draw_line
-add dl, 40
-add dh, 40
-call draw_line
-jmp wait_for_tick
-
-; =========================================== GAME LOGIC =======================
-
-the_game:
-
-
-; =========================================== DRAWING ==========================    
-
-
-mov ax, [_Y_]
-imul ax, 320
-add ax, [_X_]
-mov di, ax
-
-mov al, 0xf
-stosb
-
+draw_game:
+   call draw_cursor
+   
+   jmp wait_for_tick
 
 ; =========================================== GAME TICK ========================
 
@@ -199,8 +179,7 @@ exit:
 
 ; =========================================== PROCEDURES =======================
 
-
-draw_bg:
+prepare_intro:
    xor di, di
    mov ax, 0x9c9c             ; Set starting color
    mov dl, 0x0a               ; 10 bars to draw
@@ -211,14 +190,83 @@ draw_bg:
       xchg al, ah             ; Swap colors
       dec dl
       jnz .draw_gradient
+      
+   mov ax, 20
+   mov bx, 300
+   mov dl, 80
+   mov dh, 80
+   mov cl, 0x14
+   call draw_line
+   add dl, 40
+   add dh, 40
+   call draw_line
+
+   mov byte [_VECTOR_COLOR_], 0xf
+   mov bp, 320*81+150
+   mov si, P1XVector
+   call draw_vector
+ret
+
+prepare_game:
+   xor di, di
+   mov ax, 0x1414
+   mov cx, 320*200/2
+   rep stosw
+   mov word [_CUR_X_], 160
+   mov word [_CUR_Y_], 100
+ret
+
+draw_cursor:
+   mov ax, [_CUR_Y_]
+   imul ax, 320
+   add ax, [_CUR_X_]
+   mov di, ax
+
+   mov al, 0xf
+   stosb
+ret
+
+
+; =========================================== DRAW VECTOR ======================
+draw_vector:   
+  pusha 
+  .read_group:
+    xor cx, cx
+    mov cl, [si]
+    cmp cl, 0x0
+    jz .done
+
+    inc si
+
+    .read_line:
+    push cx
+
+    xor ax, ax
+    mov al, [si]
+    add ax, bp
+    
+    xor bx, bx
+    mov bl, [si+2]
+    add bx, bp          ; Move to position
+    mov dl, [si+1]
+    mov dh, [si+3]        
+    mov cl, [_VECTOR_COLOR_]
+    mov ch, cl
+    call draw_line
+
+    add si, 2
+    pop cx
+    loop .read_line
+    add si, 2
+    jmp .read_group
+  .done:
+  popa
 ret
 
 ; =========================================== DRAWING LINE ====================
-; ax=x0
-; bx=x1
-; dl=y0,
-; dh=y1,
-; cl=col
+; X0 - AX, Y0 - DL
+; X1 - BX, Y1 - DH
+; COLOR - CL
 ; Spektre @ https://stackoverflow.com/questions/71390507/line-drawing-algorithm-in-assembly#71391899
 draw_line:
   pusha       
@@ -263,3 +311,19 @@ draw_line:
     popa
     ret
  .ct:    dw  0
+
+
+; =========================================== DATA =============================
+
+P1XVector:
+db 4
+db 2, 35, 2, 3, 10, 3, 10, 20, 2, 20
+db 1
+db 11, 4, 14, 4
+db 1
+db 14, 35, 14, 2
+db 3
+db 18, 35, 18, 19, 24, 11, 24, 2
+db 3
+db 24, 35, 24, 19, 18, 11, 18, 2
+db 0
